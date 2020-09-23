d0c5f7a87ae9c22922bce186dadd146bf8d0007b
illegitlegilimens <61071157+illegitlegilimens@users.noreply.github.com>
2020-09-22 22:51:01 -0700

    hmwk 19

--- js/style.js
+++ js/style.js
@@ -2,12 +2,3 @@ $(".hamburger").on("click", function(){
     $("nav ul").toggleClass("active");
 });
 
-$(document).resize(function () {
-    var screen = $(window);  
-
-    if (screen.width < 1023) {
-        $('li').removeClass('button');
-    } else {
-        $('li').addClass('button');
-    }
-});
